ec38387d73e856f33a5a332b96ebd640c1fde5a3
Bastillze <36552461+Bastillze@users.noreply.github.com>
2021-11-20 07:31:07 -0500

    Second Commit

--- Book_Library_Project/Book_Library_JavaScript.js
+++ Book_Library_Project/Book_Library_JavaScript.js
@@ -1,9 +1,25 @@
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(name, author, pages, read) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return name, author, pages, read;
+  };
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(name, author, pages, read) {
+  this.newBook = function () {
+    const Book1 = new Book1(
+      "Rich Dad, Poor Dad",
+      "Robert Kiyosaki",
+      "235",
+      "Read"
+    );
+    const Book2 = new Book2("One Piece", "Eiichiro Oda", "23456", "Not Read");
+
+    const Book3 = new Book3("Rich Dad, Poor Dad", "Robert Kiyosaki, 235, Read");
+  };
 }
