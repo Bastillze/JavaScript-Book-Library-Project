9dde2354c786e01d84621780a9760ec591fb45ce
Bastillze <36552461+Bastillze@users.noreply.github.com>
2021-12-03 15:48:31 -0500

    Fifth Commit

--- Book_Library_Project/Book_Library_JavaScript.js
+++ Book_Library_Project/Book_Library_JavaScript.js
@@ -26,14 +26,37 @@ for (let i in myLibrary) {
   console.log(myLibrary[i] + " ");
 }
 
-function test() {
-  alert("Thank you for your input!");
-}
+let table = document.createElement("table");
+let thead = document.createElement("thead");
+let tbody = document.createElement("tbody");
 
-let btn = document.createElement("button");
-btn.innerHTML = "NEW BOOK";
-btn.type = "submit";
-btn.name = "formBtn";
-document.body.appendChild(btn);
+table.appendChild(thead);
+table.appendChild(tbody);
 
-function function2() {}
+document.getElementById("body").appendChild(table);
+
+let row_1 = document.createElement("tr");
+let heading_1 = document.createElement("th");
+heading_1.innerHTML = "Book";
+let heading_2 = document.createElement("th");
+heading_2.innerHTML = "Author Name";
+let heading_3 = document.createElement("th");
+heading_3.innerHTML = "Pages";
+
+row_1.appendChild(heading_1);
+row_1.appendChild(heading_2);
+row_1.appendChild(heading_3);
+thead.appendChild(row_1);
+
+let row_2 = document.createElement("tr");
+let row_2_data_1 = document.createElement("td");
+row_2_data_1.innerHTML = "One Piece";
+let row_2_data_2 = document.createElement("td");
+row_2_data_2.innerHTML = "Eiichiro Oda";
+let row_2_data_3 = document.createElement("td");
+row_2_data_3.innerHTML = "1000+";
+
+row_2.appendChild(row_2_data_1);
+row_2.appendChild(row_2_data_2);
+row_2.appendChild(row_2_data_3);
+tbody.appendChild(row_2);
